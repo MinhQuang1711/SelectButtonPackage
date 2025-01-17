fcbc3d0d3f6aa46f95dc4695303b987733e3c475
unknown <phamminhquang.work@gmail.com>
2025-01-17 11:28:34 +0700

    Fixed contrl

--- lib/select_button_package.dart
+++ lib/select_button_package.dart
@@ -69,6 +69,14 @@ class _CustomSelectButtonState<T> extends State<CustomSelectButton<T>> {
     }
   }
 
+  @override
+  void didUpdateWidget(covariant CustomSelectButton<T> oldWidget) {
+    if (oldWidget.initialValue != widget.initialValue) {
+      _controller.text = widget.initialValue ?? '';
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
   void _showBottomSheet() {
     if (widget.readOnly != true) {
       showModalBottomSheet(
